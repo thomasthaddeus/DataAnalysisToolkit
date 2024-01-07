cac1a19e3c964b8428e4f9d0fa618509d82c58f6
thomasthaddeus <thaddeus.r.thomas@gmail.com>
2024-01-06 22:37:43 -0700

    try this again

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,9 +3,9 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-import os
-import sys
-sys.path.insert(0, os.path.abspath('../src'))
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('../../src'))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
@@ -32,14 +32,14 @@ extensions = [
     'nbsphinx'
 ]
 
-templates_path = ['docs/_templates']
+templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'alabaster'
-html_static_path = ['docs/_static']
+html_static_path = ['_static']
 
 html_sidebars = {
    '**': [
